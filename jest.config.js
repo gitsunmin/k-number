b41e8f1a20f061bbdae9f16a0eeb26c7a9fab80f
김선민 <tjsals116@gmail.com>
2025-12-04 23:19:47 +0900

    📝 add moduleNameMapper to jest configuration for improved module resolution

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/test/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
